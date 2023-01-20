2e8224a6c066aca6542a5546c5c1d4848077ebb9
AntonKrasouski88 <antohkad21@gmail.com>
2023-01-20 14:51:39 +0300

    hw-6 localstorage

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        let state = restoreState<string>('hw6-editable-span-value', '');
+        setValue(state);
     }
 
     return (
