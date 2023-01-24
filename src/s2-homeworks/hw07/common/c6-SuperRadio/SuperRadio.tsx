058f81f3895bd640a64155aef42ae916fc0ee261
AntonKrasouski <antohkad21@gmail.com>
2023-01-24 23:16:38 +0300

    hw-7 did onChangeCallBack in components SuperRadio and SuperSelect

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        onChange?.(e);
+        onChangeOption?.(+e.currentTarget.value);
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,8 +50,11 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
-                      // name, checked, value делают студенты
+                      name={o.name}
+                      value={o.id}
+                      checked={value === o.id}
 
+                      // name, checked, value делают студенты
                       onChange={onChangeCallback}
                       {...restProps}
                   />
